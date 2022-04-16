7f3f3ccb5b35bdbae2786feb6436f2c0139b3741
gabriel santos freitas <gabrielbh@hotmail.com>
2022-04-16 00:34:56 -0300

    fazendo a pagina index / login

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  
+<>
+
+<Component {...pageProps} />
+
+<style global jsx>
+  {`
+  
+  body{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+    
+  
+  
+  `}
+</style>
+
+</>
+
+  
+  )
 }
 
 export default MyApp
